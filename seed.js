aac1d6ac937f6046fd7554b105ede48c8a93a4de
Anthony Lopez <Antl21292@gmail.com>
2023-02-11 18:50:55 -0500

    seed created, 3 books added

---
+++ seed.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGODB_URL);
+
+const Book = require('./schema/book');
+
+const seed = async () => {
+
+  await Book.create({
+    title: 'Harry Potter',
+    desc: 'A famous wizard with a scar on his forehead.',
+    status: false,
+  })
+  .then(book => console.log('Saved Harry Potter to Database'))
+  .catch(error => console.error(error));
+
+  await Book.create({
+    title: 'Hunger Games',
+    desc: 'Survival of the fitest!',
+    status: true,
+  })
+  .then(book => console.log('Saved Hunger Games to Database'))
+  .catch(error => console.error(error));
+
+  await Book.create({
+    title: 'The Shadow of the Wind',
+    desc: 'A story within a story.',
+    status: true,
+  })
+  .then(book => console.log('Saved The Shadow of the Wind to Database'))
+  .catch(error => console.error(error));
+}
+
+mongoose.disconnect;
+
+seed();
