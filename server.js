76beec20233f292ea66e554aba092313606edff0
Anthony Lopez <Antl21292@gmail.com>
2023-02-15 19:49:13 -0500

    imported new function and created post route

--- server.js
+++ server.js
@@ -4,15 +4,20 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const getBooks = require('./models/getBooks');
+// const getBooks = require('./books/getBooks'); - no longer needed because bookHandler contains our function
+const bookHandler = require('./modules/handleCreateBook')
+
 
 const app = express();
 app.use(cors());
 
+//using express.json to be able to use req.body data
+app.use(express.json());
 
 
 const PORT = process.env.PORT || 3002;
 
+mongoose.set('strictQuery', false);
 mongoose.connect(process.env.MONGODB_URL);
 
 const db = mongoose.connection;
@@ -23,6 +28,7 @@ db.once('open', () => console.log('Connected to Mongoose'));
 
 app.get('/', (req, res) => res.status(200).send('Welcome to our Library'));
 
-app.get('/books', getBooks)
+app.get('/books', bookHandler.getBooks);
+app.post('/books', bookHandler.postBooks);
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
